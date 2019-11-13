8d7b79f692a76100976ed7ab466713132a8e35f6
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2019-11-13 09:57:53 -0600

    "final version"

--- template.js
+++ template.js
@@ -2,9 +2,9 @@ const STORE = {
     projects: `<div class="contain-content">
     <img src="Images/quiz-app-screen.png" class="quiz-screen" alt="Mackinac quiz app screenshot"/>
     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My first project using Javascript and jQuery, this quiz app allows you to test your knowledge <br>
-    about an obscure island in the Great Lakes. This was inspired by a summer I spent working there. <br>
-    Utilizing:</p>
+    about an obscure island in the Great Lakes. This was inspired by a summer I spent working there. <br></p>
         <div class="languages">
+            <p>Utilizing:</p>
             <img src="Images/html5.png" class="lang-icon" alt="HTML5 icon"></a>
             <img src="Images/css3.png" class="lang-icon" alt="CSS3 icon"></a>
             <img src="Images/javascript-icon.png" class="lang-icon" alt="Javascript icon"></a>
