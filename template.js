7a7521b58c28ea75d2345357ea29972c960e5901
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2019-11-13 08:57:05 -0600

    "image rendering fixed"

--- template.js
+++ template.js
@@ -1,14 +1,14 @@
 const STORE = {
     projects: `<div class="contain-content">
-    <img src="images/quiz-app-screen.png" class="quiz-screen" alt="Mackinac quiz app screenshot"/>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; My first project using Javascript and jQuery, this quiz app allows you to test your knowledge <br>
+    <img src="Images/quiz-app-screen.png" class="quiz-screen" alt="Mackinac quiz app screenshot"/>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My first project using Javascript and jQuery, this quiz app allows you to test your knowledge <br>
     about an obscure island in the Great Lakes. This was inspired by a summer I spent working there. <br>
     Utilizing:</p>
         <div class="languages">
-            <img src="images/html5.png" class="lang-icon" alt="HTML5 icon"></a>
-            <img src="images/css3.png" class="lang-icon" alt="CSS3 icon"></a>
-            <img src="images/javascript-icon.png" class="lang-icon" alt="Javascript icon"></a>
-            <img src="images/jQuery.png" class="lang-icon" alt="jQuery icon"></a>
+            <img src="Images/html5.png" class="lang-icon" alt="HTML5 icon"></a>
+            <img src="Images/css3.png" class="lang-icon" alt="CSS3 icon"></a>
+            <img src="Images/javascript-icon.png" class="lang-icon" alt="Javascript icon"></a>
+            <img src="Images/jQuery.png" class="lang-icon" alt="jQuery icon"></a>
         </div>
     <a href="https://cjenkins14.github.io/Mack-quiz-app/
     ">Live Site</a>
@@ -20,14 +20,14 @@ const STORE = {
     my career, I had to decide what type of problem I wanted to handle in my everyday life. Some of my natural 
     skills are attention to detail, an eye for efficiency, and being an avid learner. I felt that these would 
     shine within the development field. I enjoy coding because solving problems, and having the benefit of seeing 
-    my work come to life is exciting. When I am not losing myself in learning a new language or project, I am likely "
+    my work come to life is exciting. When I am not losing myself in learning a new language or project, I am likely 
     traveling, enjoying time outdoors, or writing poetry.</p>
       <div class="contact">
         <p>If you have any questions, or are interested in how I may help you, please feel free to contact me via one of these options:</p>
           <div class="contact-links">
-            <a href = "mailto:coreyjenkins24@gmail.com?subject = Feedback&body = Message"><img src="images/email.png"/></a>
-            <a href="https://www.linkedin.com/in/coreyjenkins2414"><img src="images/linkedin (1).png"/></a>
-            <a href="https://github.com/Cjenkins14"><img src="images/github-icon.png"/></a>
+            <a href = "mailto:coreyjenkins24@gmail.com?subject = Feedback&body = Message"><img src="Images/email.png"/></a>
+            <a href="https://www.linkedin.com/in/coreyjenkins2414"><img src="Images/linkedin (1).png"/></a>
+            <a href="https://github.com/Cjenkins14"><img src="Images/github-icon.png"/></a>
           </div>
       </div>
     </div>
