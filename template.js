eb01c03d2106a7d165d4c3e30a819c3669da53f9
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2019-11-20 07:10:32 -0600

    "revisit"

--- template.js
+++ template.js
@@ -1,35 +1,38 @@
 const STORE = {
     projects: `<div class="contain-content">
-    <img src="Images/quiz-app-screen.png" class="quiz-screen" alt="Mackinac quiz app screenshot"/>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My first project using Javascript and jQuery, this quiz app allows you to test your knowledge
-    about an obscure island in the Great Lakes. This was inspired by a summer I spent working there.</p>
-        <div class="languages">
-            <p>Utilizing:</p>
-            <img src="Images/html5.png" class="lang-icon" alt="HTML5 icon"></a>
-            <img src="Images/css3.png" class="lang-icon" alt="CSS3 icon"></a>
-            <img src="Images/javascript-icon.png" class="lang-icon" alt="Javascript icon"></a>
-            <img src="Images/jQuery.png" class="lang-icon" alt="jQuery icon"></a>
+    <h2>Mackinac Island quiz app</h2>
+    <img src="Images/mack-quiz-screenshot.png" class="quiz-screen" alt="Mackinac quiz app screenshot"/>
+    <h3>Utilizing:</h3>
+          <div class="languages">
+              <img src="Images/html5.png" class="lang-icon" alt="HTML5 icon"></a>
+              <img src="Images/css3.png" class="lang-icon" alt="CSS3 icon"></a>
+              <img src="Images/javascript-icon.png" class="lang-icon" alt="Javascript icon"></a>
+              <img src="Images/jQuery.png" class="lang-icon" alt="jQuery icon"></a>
+          <div class="links">
+            <a href="https://cjenkins14.github.io/Mack-quiz-app/" target="_blank">Live Site</a>
+            <a href="https://github.com/Cjenkins14/Mack-quiz-app" target="_blank">Code</a>
+          </div>
         </div>
-    <a href="https://cjenkins14.github.io/Mack-quiz-app/
-    ">Live Site</a>
-    <a href="https://github.com/Cjenkins14/Mack-quiz-app
-    ">Code</a></div>`,
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;My first project using Javascript and jQuery, this quiz app allows you to test your knowledge
+    about an obscure island in the Great Lakes. This was inspired by a summer I spent working there, after I learned that most people
+    outside the state don't know it exists.</p></div>`,
     about: ` <div class="contain-content">
     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; A few years ago, I completed my first Raspberry Pi project using Python. 
     I became very interested in what I could accomplish if I knew how to write software. Some of my natural skills 
     are attention to detail, having an eye for efficiency, and being an avid learner. I felt that these would shine 
-    within the development field. I enjoy coding because solving problems and seeing my solutions come to life is exciting. 
+    within the development field. I enjoy coding because solving problems and seeing my solutions come to life is exciting. I am
+    ready to work with a team of great programmers and excited for what I can learn.
     When I am not losing myself in learning a new language or project I am likely traveling, enjoying time outdoors, or reading.</p>
-      <div class="contact">
-        <p>Please feel free to contact me:</p>
-          <div class="contact-links">
-            <a href = "mailto:coreyjenkins24@gmail.com?subject = Feedback&body = Message"><img src="Images/email.png"/></a>
-            <a href="https://www.linkedin.com/in/coreyjenkins2414"><img src="Images/linkedin (1).png"/></a>
-            <a href="https://github.com/Cjenkins14"><img src="Images/github-icon.png"/></a>
-          </div>
-      </div>
     </div>
     `,
+    contact: `<div class="contact">
+    <p>Please feel free to contact me:</p>
+      <div class="contact-links">
+        <a href = "mailto:coreyjenkins24@gmail.com?subject = Feedback&body = Message"><img src="Images/email.png"/></a>
+        <a href="https://www.linkedin.com/in/coreyjenkins2414"><img src="Images/linkedin (1).png"/></a>
+        <a href="https://github.com/Cjenkins14"><img src="Images/github-icon.png"/></a>
+      </div>
+  </div>`,
     Headline: `<div class="contain-content">
     <pHello, I’m Corey, a full-stack developer that enjoys working with front-end web design and interactive programming. </p>
     <p>
