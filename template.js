a0e2a49ade5ee55b5d447d90f40d6921d99a29ea
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2020-04-09 10:03:15 -0500

    added packit project and screenshot image

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
 const STORE = {
-    projects: `<div class="contain-content">
+  projects: `<div class="contain-content">
     <h2>Mackinac Island quiz app</h2>
     <img src="Images/mack-quiz-screenshot.png" class="quiz-screen" alt="Mackinac quiz app screenshot"/>
         <div class="languages">
@@ -14,8 +14,26 @@ const STORE = {
         </div>
     <p>My first project using Javascript and jQuery, this quiz app allows you to test your knowledge
     about an obscure island in the Great Lakes. This was inspired by a summer I spent working there, after I learned that most people
-    outside the state don't know it exists.</p></div>`,
-    about: ` <div class="contain-content">
+    outside the state don't know it exists.</p> <br />
+    <h2>PackIt traveling app</h2>
+    <img src="Images/packit-result-screen.png" class="quiz-screen" alt="PackIt app screenshot"/>
+        <div class="languages">
+              <img src="Images/html5.png" class="lang-icon" alt="HTML5 icon"></a>
+              <img src="Images/css3.png" class="lang-icon" alt="CSS3 icon"></a>
+              <img src="Images/javascript-icon.png" class="lang-icon" alt="Javascript icon"></a>
+              <img src="Images/jQuery.png" class="lang-icon" alt="jQuery icon"></a>
+          <div class="links">
+            <a href="https://cjenkins14.github.io/packIt-app/" target="_blank">Demo</a>
+            <a href="https://github.com/Cjenkins14/packIt-app" target="_blank">Code</a>
+          </div>
+        </div>
+    <p>PackIt was designed to help travelers by creating a checklist for their packing needs. 
+    The app uses the weatherBit API to find the weather conditions around the globe, and renders a packing list based upon those weather conditions. 
+    All that is needed is the postal code of the area you are traveling to and the dates that you will be there, the app does the rest. 
+</p>
+    </div>`,
+
+  about: ` <div class="contain-content">
     <p>A few years ago, I completed my first Raspberry Pi project using Python. 
     I became very interested in what I could accomplish if I knew how to write software. Some of my natural skills 
     are attention to detail, having an eye for efficiency, and being an avid learner. I felt that these would shine 
@@ -24,15 +42,15 @@ const STORE = {
     When I am not losing myself in learning a new language or project I am likely traveling, enjoying time outdoors, or reading.</p>
     </div>
     `,
-    contact: `<div class="contain-content">
-    <p>I am currently searching for a position where I can bring my strengths of <span>efficiency</span>, <span>attention to detail</span> and love to <span>learn</span> if you have any questions or would like to speak to me regarding my work, please feel free to contact me:</p>
+  contact: `<div class="contain-content">
+    <p>I am currently searching for a position where I can bring my strengths of <span>efficiency</span>, <span>attention to detail</span> and love to <span>learn</span>. if you have any questions or would like to speak to me regarding my work, please feel free to contact me:</p>
       <div class="contact-links">
         <a href = "mailto:coreyjenkins24@gmail.com?subject = Feedback&body = Message" target="_blank"><img src="Images/email.png"/></a>
         <a href="https://www.linkedin.com/in/coreyjenkins2414" target="_blank"><img src="Images/linkedin (1).png"/></a>
         <a href="https://github.com/Cjenkins14" target="_blank"><img src="Images/github-icon.png"/></a>
       </div>
   </div>`,
-    Headline: `<div class="contain-content">
+  Headline: `<div class="contain-content">
     <pHello, I’m Corey, a full-stack developer that enjoys working with front-end web design and interactive programming. </p>
     <p>
         page content, images and description
